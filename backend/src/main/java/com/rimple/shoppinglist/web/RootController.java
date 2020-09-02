87b80a21a028efb7677ec97ef3cf0752740aa36e
ken Rimple <krimple@chariotsolutions.com>
2020-09-01 21:32:10 -0400

    added root controller for healthcheck

---
+++ backend/src/main/java/com/rimple/shoppinglist/web/RootController.java
@@ -0,0 +1,14 @@
+package com.rimple.shoppinglist.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class RootController {
+    @GetMapping
+    public String healthCheck() {
+        return "OK";
+    }
+}
